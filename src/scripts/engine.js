9fc9e923e4b811897f033a3b58cdba7960a7a52b
Gilead NS <gfrais.gpf@gmail.com>
2024-11-11 09:59:24 -0300

    Criando elementos javascript

---
+++ src/scripts/engine.js
@@ -0,0 +1,27 @@
+const emojis = [
+    "😾",
+    "😾",
+    "🦝",
+    "🦝",
+    "🐵",
+    "🐵",
+    "🦊",
+    "🦊",
+    "🐰",
+    "🐰",
+    "🐷",
+    "🐷",
+    "🐼",
+    "🐼",
+];
+let openCards = [];
+
+
+let shuffleEmojis = emojis.sort(() =>(Math.random() > 0.5 ? 2 : -1));
+
+for(let i=0; i < emojis.length; i++){
+    let box = document.createElement("div");
+    box.className = "item";
+    box.innerHTML = shuffleEmojis[i];
+    document.querySelector(".game").appendChild(box)
+}
